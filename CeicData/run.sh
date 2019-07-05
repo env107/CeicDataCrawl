bba993181b03c4f032f322d8a3e6eb24a092d9a2
env107 <env107@126.com>
2019-07-05 14:50:52 +0800

    Update run.sh

--- CeicData/run.sh
+++ CeicData/run.sh
@@ -2,9 +2,9 @@
 
 echo "开始进行爬虫..."
 cd /home/admin/python/CeicDataCrawl/CeicData #进入目录
-time=`date +%Y%m%d%H%I%S`
-py3=`which python3`
-$py3 /home/admin/python/CeicDataCrawl/CeicData/main.py >> /home/admin/python/CeicDataCrawl/CeicData/log/pull_$time.log
+# time=`date +%Y%m%d%H%I%S`
+
+/usr/local/bin/python3 /home/admin/python/CeicDataCrawl/CeicData/main.py > /home/admin/python/CeicDataCrawl/CeicData/log/parse.log
 
 echo "爬虫结束"
 
