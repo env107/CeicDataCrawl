9da2768b137ba0a2673d553546f4586aead934e0
env107 <env107@126.com>
2019-07-05 14:46:13 +0800

    1.6
    优化显示打印

--- CeicData/pipelines.py
+++ CeicData/pipelines.py
@@ -9,6 +9,7 @@ import pymysql
 import base64
 import traceback
 import pickle
+import time
 
 class CeicdataPipeline(object):
     def process_item(self, item, spider):
@@ -17,23 +18,31 @@ class CeicdataPipeline(object):
         #首先查找hash值是否存在
         hashstr = base64.b64encode(pickle.dumps(item))
         cursor = db.cursor()
-        checkSql = 'SELECT count(`hash`) as length FROM `current_data` WHERE `hash`=%s'
+        checkSql = 'SELECT id as length FROM `current_data` WHERE `hash`=%s'
         cursor.execute(checkSql,(hashstr))
         checkData = cursor.fetchone()
-        if(checkData[0] < 1 ):
-            print("开始写入数据")
-            print(item)
+        if(checkData == None):
+            self.show_time("准备写入数据")
+            self.show_time(item)
             #写入数据库
             sql = 'INSERT INTO `current_data`(level,happenTime,lat,lon,deep,position,hash) VALUES(%s,%s,%s,%s,%s,%s,%s)'
             
             try:
                 cursor.execute(sql,(item['level'],item['time'],item['lat'],item['lon'],item['deep'],item['position'],hashstr))
                 db.commit()
+                self.show_time("写入数据成功")
             except BaseException:
+                self.show_time("处理程序出现错误")
                 print(traceback.format_exc())
                 db.rollback()
+            
+        else:
+            self.show_time("数据["+str(checkData[0])+"]已存在!")
         pass
 
         db.close()
 
         return item
+
+    def show_time(self,text):
+        print ("#",str(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())),"-> ",text)
--- CeicData/spiders/action.py
+++ CeicData/spiders/action.py
@@ -1,6 +1,7 @@
 import scrapy
 from CeicData.items import CeicdataItem
 import sys
+import time
 
 class action(scrapy.Spider):
 
@@ -9,14 +10,15 @@ class action(scrapy.Spider):
     start_urls = ['http://news.ceic.ac.cn/index.html']
 
     def parse(self , response):
-        print ("======= 开始挖掘数据 =======")
+        self.show_time("开始解析数据工作")
         #爬取每一行地震内容
         rawData = response.xpath("//div[@class='news-content']/table//tr[position()>1]")
-
+        rawline = 0
         dataItem = CeicdataItem()
         
         #获取每一行内容
         for raw in rawData:
+            rawline = rawline+1
             info = raw.xpath(".//td//text()").extract()
             dataItem['level'] = info[0]
             dataItem['time'] = info[1]
@@ -24,14 +26,17 @@ class action(scrapy.Spider):
             dataItem['lon'] = info[3]
             dataItem['deep'] = info[4]
             dataItem['position'] = info[5]
-            print ("======= 已解析数据 =======")
+            self.show_time("抛出第"+str(rawline)+"行数据")
              ##抛出生成器
             yield dataItem
         pass
 
        
         
-        print ("======= 数据解析结束 =======")
+        self.show_time("数据解析结束!")
+
+    def show_time(self,text):
+        print ("#",str(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())),"-> ",text)
 
 
 
