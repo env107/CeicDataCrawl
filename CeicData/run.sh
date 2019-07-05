7afdc57c1f7eb56ce4d33814603eefa102e76783
env107 <env107@126.com>
2019-07-05 11:19:18 +0800

    1.32
    修改脚本

--- CeicData/run.sh
+++ CeicData/run.sh
@@ -2,8 +2,9 @@
 
 echo "开始进行爬虫..."
 #/home/admin/python/CeicDataCrawl/CeicData
-time=date +%Y%m%d%H%i%s
-python3 /home/admin/python/CeicData/CeicData/main.py > log/pull_$time.log
+time=`date +%Y%m%d%H%i%s`
+py3=`which python3`
+$py3 /home/admin/python/CeicData/CeicData/main.py > log/pull_$time.log
 
 echo "爬虫结束"
 
